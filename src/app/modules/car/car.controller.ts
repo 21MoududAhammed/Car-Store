6f77a1bf61d5d321ff862522076ae8c8d45762ca
21MoududAhammed <21moudud@gmail.com>
2024-11-23 17:59:01 +0600

    zod validation implemented before create a car to the database

--- src/app/modules/car/car.controller.ts
+++ src/app/modules/car/car.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 import { carServices } from './car.service';
-
+import { carZodSchema } from './car.validation';
+import { ZodError } from 'zod';
+// create a car
 const createSingleCar = async (req: Request, res: Response) => {
   try {
-    const carData = req.body;
+    const carData = carZodSchema.parse(req.body);
     const result = await carServices.createSingleCarService(carData);
     res.status(200).json({
       success: true,
@@ -11,7 +13,20 @@ const createSingleCar = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (err) {
-    console.log(err);
+    if (err instanceof ZodError) {
+      // handle validation errors
+      return res.status(400).json({
+        success: false,
+        message: 'Validation Failed',
+        errors: err,
+      });
+    }
+    // handle other errors
+    res.status(500).json({
+      success: false,
+      message: 'An unexpected error occurred.',
+      errors: err,
+    });
   }
 };
 
--- src/app/modules/car/car.validation.ts
+++ src/app/modules/car/car.validation.ts
@@ -1,24 +1,30 @@
 import { z } from 'zod';
 
 export const carZodSchema = z.object({
-  brand: z.string().nonempty('Brand is required'), // Non-empty string
-  model: z.string().nonempty('Model is required'),
+  brand: z
+    .string({ required_error: 'Brand is required' })
+    .nonempty('Brand is required'), // Non-empty string
+  model: z
+    .string({ required_error: 'Model is required' })
+    .nonempty('Model is required'),
   year: z
-    .number()
+    .number({ required_error: 'Year is required' })
     .int('Year must be an integer')
     .min(1886, 'Year must be at least 1886') // First car invented in 1886
     .max(
       new Date().getFullYear(),
       `Year cannot exceed ${new Date().getFullYear()}`,
     ),
-  price: z.number().positive('Price must be a positive number'),
+  price: z
+    .number({ required_error: 'Price is required' })
+    .positive('Price must be a positive number'),
   category: z.enum(['Sedan', 'SUV', 'Truck', 'Coupe', 'Convertible'], {
     required_error: 'Category is required',
   }),
   description: z.string().nonempty('Description is required'),
   quantity: z
-    .number()
+    .number({ required_error: 'Quantity is required' })
     .int('Quantity must be an integer')
     .min(0, 'Quantity cannot be negative'),
-  inStock: z.boolean(),
+  inStock: z.boolean({ required_error: 'In stock is required' }),
 });
